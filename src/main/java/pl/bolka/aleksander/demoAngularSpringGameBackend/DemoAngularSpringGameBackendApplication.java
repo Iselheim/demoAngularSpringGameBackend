a9ea6fe34ffa900362edac119f1cd289037d3139
abolka <bolka.aleksander@gmail.com>
2018-04-22 15:49:01 +0200

    Initial commit

---
+++ src/main/java/pl/bolka/aleksander/demoAngularSpringGameBackend/DemoAngularSpringGameBackendApplication.java
@@ -0,0 +1,12 @@
+package pl.bolka.aleksander.demoAngularSpringGameBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAngularSpringGameBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAngularSpringGameBackendApplication.class, args);
+	}
+}
